28ba49b1ac04db5ff3cfa1546252c47861b1a6a2
Anton Nikolski <ant.nikolski@gmail.com>
2023-04-06 15:59:46 +0300

    exclude build

--- src/test/java/github/SelenideRepositorySearch.java
+++ src/test/java/github/SelenideRepositorySearch.java
@@ -1,17 +1,24 @@
 package github;
 
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
-import static com.codeborne.selenide.Selenide.open;
 
+import static com.codeborne.selenide.Selenide.*;
+
+@SuppressWarnings("doesn't include 'test'")
 public class SelenideRepositorySearch {
 
     @Test
     void shouldFindSelenideRepositoryAtTheTop() {
         //открыть главную страницу
-        open();
+        open("https://github.com/");
         //ввести в поле поиска Selenide и нажать enter
+        $("[placeholder='Search GitHub'").setValue("Selenide").pressEnter();
         //кликнуть на первый найденный репозиторий
+        $$("ul.repo-list li").first().click();
         //проверка: заголовок "selenide/selenide"
+
+        sleep(6000);
 
     }
 
