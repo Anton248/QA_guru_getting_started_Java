d9b9ee1f127e1d473c3c30dd7d720fe8f1415523
Anton Nikolski <ant.nikolski@gmail.com>
2023-04-12 00:44:19 +0300

    difference class

--- src/test/java/github/DifferenceBetweenTwoExpressions.java
+++ src/test/java/github/DifferenceBetweenTwoExpressions.java
@@ -1,16 +1,26 @@
 package github;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class DifferenceBetweenTwoExpressions {
     @Test
-    void findDivAndH2() {
+    void withAndWithoutDiv() {
         Configuration.holdBrowserOpen = true;
-        open("https://github.com/features/actions");
-        $("div.d-flex a.Link--primary[href='/features/packages']").click(); //works
-        $("div.d-flex").$("a.Link--primary[href='/features/packages']").click();  //doesn't work
+        open("https://github.com/selenide/selenide");
+
+        //works
+        $("div.Layout-sidebar").$(byText("Contributors"))
+                .closest(".BorderGrid-cell").$$("ul li").first().hover();
+
+        //doesn't work
+        $("div.Layout-sidebar").$(byText("Contributors"))
+                .closest("div.BorderGrid-cell").$$("ul li").first().hover();
     }
 }
